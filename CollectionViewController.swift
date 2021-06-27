6f7e2ebab7a3fbad117615abee1eccf0ee0a3cb6
xionglong.xu <xionglong.xu@gmail.com>
2021-06-27 14:04:17 +0800

    UICollectionView reordering

--- CollectionViewController.swift
+++ CollectionViewController.swift
@@ -3,13 +3,23 @@ import UIKit
 
 private class ViewCell: UICollectionViewCell {
     let label = UILabel()
+    let button = UIButton()
     override init(frame: CGRect) {
         super.init(frame: frame)
         label.backgroundColor = UIColor.yellow
         label.textColor = UIColor.black
         contentView.addSubview(label)
+        
+        button.backgroundColor = UIColor.red
+        contentView.addSubview(button)
+        
         label.snp.makeConstraints { (make) in
-            make.edges.equalToSuperview()
+            make.left.top.bottom.equalToSuperview()
+            make.right.equalToSuperview().inset(10)
+        }
+        button.snp.makeConstraints { (make) in
+            make.left.equalTo(label.snp.right)
+            make.top.bottom.right.equalToSuperview()
         }
     }
     
@@ -19,6 +29,8 @@ private class ViewCell: UICollectionViewCell {
 }
 
 class CollectionViewController: UIViewController {
+    var collectionView: UICollectionView!
+    
     init() {
         super.init(nibName: nil, bundle: nil)
     }
@@ -33,18 +45,24 @@ class CollectionViewController: UIViewController {
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .horizontal
         
-        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
         collectionView.register(ViewCell.self, forCellWithReuseIdentifier: "id")
+        //weak reference
         collectionView.delegate = self
         collectionView.dataSource = self
         collectionView.backgroundColor = UIColor.blue
         collectionView.isPagingEnabled = false
+        collectionView.showsHorizontalScrollIndicator = false
         view.addSubview(collectionView)
+        
+        let pan = UIPanGestureRecognizer(target: self, action: #selector(onPan(_:)))
+        pan.delegate = self
+        collectionView.addGestureRecognizer(pan)
         
         collectionView.snp.makeConstraints { (make) in
             make.left.right.equalToSuperview()
             make.centerY.equalToSuperview()
-            make.height.equalTo(110)
+            make.height.equalTo(150)
         }
     }
 }
@@ -52,7 +70,7 @@ class CollectionViewController: UIViewController {
 extension CollectionViewController: UICollectionViewDelegateFlowLayout {
     //UICollectionViewFlowLayout.sectionInset
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        return UIEdgeInsets(top: 1, left: 1, bottom: 1, right: 1)
+        return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
     }
     
     //UICollectionViewFlowLayout.itemSize
@@ -69,6 +87,7 @@ extension CollectionViewController: UICollectionViewDataSource {
     
     //required
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        print("cellForItemAt \(indexPath.item)")
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "id", for: indexPath) as! ViewCell
         cell.label.text = String(indexPath.item)
         return cell
@@ -79,10 +98,50 @@ extension CollectionViewController: UICollectionViewDataSource {
     }
 
     func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
-        print("willDisplay \(indexPath.item) ")
+        print("willDisplay \(indexPath.item)")
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         print("didSelectItemAt \(indexPath.item)")
+    }
+    
+    //reordering
+    func collectionView(_ collectionView: UICollectionView, canMoveItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    func collectionView(_ collectionView: UICollectionView, moveItemAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        
+    }
+    
+}
+
+extension CollectionViewController {
+    @objc func onPan(_ gesture: UIPanGestureRecognizer) {
+        switch gesture.state {
+        case .began:
+            //Gets the index path of the item at the specified point in the collection view.
+            if let indexPath = collectionView.indexPathForItem(at: gesture.location(in: collectionView)) {
+                collectionView.beginInteractiveMovementForItem(at: indexPath)
+            }
+        case .changed:
+            collectionView.updateInteractiveMovementTargetPosition(gesture.location(in: collectionView))
+        case .ended:
+            collectionView.endInteractiveMovement()
+        default:
+            collectionView.cancelInteractiveMovement()
+        }
+    }
+}
+
+//gesture conflicting
+extension CollectionViewController: UIGestureRecognizerDelegate {
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldReceive touch: UITouch) -> Bool {
+        if let indexPath = collectionView.indexPathForItem(at: touch.location(in: collectionView)) {
+            let cell = collectionView.cellForItem(at: indexPath) as! ViewCell
+            if (cell.button.bounds.contains(touch.location(in: cell.button))) {
+                return true
+            }
+        }
+        return false
     }
 }
